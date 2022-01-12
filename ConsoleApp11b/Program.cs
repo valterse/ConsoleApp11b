c4bd33b4e6ccedb916df1547651150b574fdb3e4
valterse <79447676+valterse@users.noreply.github.com>
2022-01-12 08:39:46 +0200

    Add project files.

---
+++ ConsoleApp11b/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zdarova no valtera\n")
+        }
+    }
+}
